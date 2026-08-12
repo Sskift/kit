2f9383af3ff02679ba319b23d9952f0d9bd3d115
Ottomated <otto@ottomated.net>
2026-08-11 20:31:03 -0700

    support Request.cf

--- packages/adapter-cloudflare/index.js
+++ packages/adapter-cloudflare/index.js
@@ -14,6 +14,7 @@ import {
 	append_headers
 } from './utils.js';
 import { exactRegex } from '@rolldown/pluginutils';
+import { getRequest } from '@sveltejs/kit/node';
 
 const name = '@sveltejs/adapter-cloudflare';
 
@@ -190,6 +191,13 @@ export default function (options = {}) {
 		supports: {
 			read: () => true,
 			instrumentation: () => true
+		},
+		getRequest(options) {
+			const request = getRequest(options);
+			/** @type {import('@cloudflare/workers-types').Request} */ (
+				/** @type {unknown} */ (request)
+			).cf = globalThis.__platform_proxy?.cf;
+			return request;
 		},
 		vite: {
 			plugins: {
